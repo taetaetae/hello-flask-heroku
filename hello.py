614a87622719ac1b046ffad3385af2dd1c66360c
taetaetae <taetaetae_@naver.com>
2022-02-12 01:33:56 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
 @app.route('/meme', methods=['GET'])
 def meme():
     id = request.args.get('id')
