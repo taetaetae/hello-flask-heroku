dfbd72d38405ef78674ff76045accd263308de05
taetaetae <taetaetae_@naver.com>
2022-02-13 02:20:24 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello dduzi!'
 
 @app.route('/meme', methods=['GET'])
 def meme():
@@ -38,5 +38,35 @@ def meme():
                     maxArticle = article
         page = page + 1
     return str(min)
+
+@app.route('/jeonse', methods=['GET'])
+def jeonse():
+    id = request.args.get('id')
+
+    max = 0
+    page = 1
+    minArticle = []
+
+    while True:
+        if minArticle :
+            break
+        resposne = requests.get(
+            url=f"https://m.land.naver.com/complex/getComplexArticleList?hscpNo={id}&tradTpCd=B1&order=prc_&page={page}",
+            headers={
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36"
+            }
+        ).json()
+
+        for article in resposne['result']['list']:
+            if "84" in article['spc2'] :
+                dealOrWarrantPrc = str(article['prcInfo']).split('억')
+                firstPrc = int(dealOrWarrantPrc[0]) * 10000
+                secondPrc = 0 if dealOrWarrantPrc[1] == '' else int(str(dealOrWarrantPrc[1]).replace(",", ""))
+                if max < firstPrc + secondPrc:
+                    max = firstPrc + secondPrc
+                    minArticle = article
+        page = page + 1
+    return str(max)
+
 if __name__ == '__main__':
     app.run()
