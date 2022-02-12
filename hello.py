bd80e3a2eb9ec6148554504cafcfd3fec678793a
taetaetae <taetaetae_@naver.com>
2022-02-13 02:43:25 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -13,30 +13,30 @@ def meme():
 
     min = sys.maxsize
     page = 1
-    maxArticle = []
+    minArticle= []
 
     while True:
-        if maxArticle :
+        if minArticle :
             break
         resposne = requests.get(
-            url=f"https://m.land.naver.com/complex/getComplexArticleList?hscpNo={id}&tradTpCd=A1&order=prc&page={page}",
+            url=f"https://new.land.naver.com/api/articles/complex/{id}?realEstateType=APT%3AABYG%3AJGC&tradeType=A1&tag=%3A%3A%3A%3A%3A%3A%3A%3A&rentPriceMin=0&rentPriceMax=900000000&priceMin=0&priceMax=900000000&areaMin=0&areaMax=900000000&oldBuildYears&recentlyBuildYears&minHouseHoldCount&maxHouseHoldCount&showArticle=false&sameAddressGroup=false&minMaintenanceCost&maxMaintenanceCost&priceType=RETAIL&directions=&page={page}&complexNo={id}&buildingNos=&type=list&order=prc",
             headers={
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36",
-                "Referer": "https://m.land.naver.com/"
+                "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlJFQUxFU1RBVEUiLCJpYXQiOjE2NDQ1ODY5NzIsImV4cCI6MTY0NDU5Nzc3Mn0.jpa_rrmB3Hs2xwqQSfbgPRWcoYq0Bz5i4QUnDRrUYBE"
             }
         ).json()
 
-        for article in resposne['result']['list']:
-            if "84" in article['spc2'] :
-                floor = str(article['flrInfo']).split("/")[0]
+        for article in resposne['articleList']:
+            if article['area2'] == 84:
+                floor = str(article['floorInfo']).split("/")[0]
                 if floor.isdigit() and int(floor) < 5:
                     continue
-                dealOrWarrantPrc = str(article['prcInfo']).split('억')
+                dealOrWarrantPrc = str(article['dealOrWarrantPrc']).split('억')
                 firstPrc = int(dealOrWarrantPrc[0]) * 10000
                 secondPrc = 0 if dealOrWarrantPrc[1] == '' else int(str(dealOrWarrantPrc[1]).replace(",", ""))
                 if min > firstPrc + secondPrc:
                     min = firstPrc + secondPrc
-                    maxArticle = article
+                    minArticle = article
         page = page + 1
     return str(min)
 
@@ -46,27 +46,27 @@ def jeonse():
 
     max = 0
     page = 1
-    minArticle = []
+    maxArticle = []
 
     while True:
-        if minArticle :
+        if maxArticle:
             break
         resposne = requests.get(
-            url=f"https://m.land.naver.com/complex/getComplexArticleList?hscpNo={id}&tradTpCd=B1&order=prc_&page={page}",
+            url=f"https://new.land.naver.com/api/articles/complex/{id}?realEstateType=APT%3AABYG%3AJGC&tradeType=B1&tag=%3A%3A%3A%3A%3A%3A%3A%3A&rentPriceMin=0&rentPriceMax=900000000&priceMin=0&priceMax=900000000&areaMin=0&areaMax=900000000&oldBuildYears&recentlyBuildYears&minHouseHoldCount&maxHouseHoldCount&showArticle=false&sameAddressGroup=false&minMaintenanceCost&maxMaintenanceCost&priceType=RETAIL&directions=&page={page}&complexNo={id}&buildingNos=&type=list&order=prcDesc",
             headers={
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36",
-                "Referer": "https://m.land.naver.com/"
+                "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlJFQUxFU1RBVEUiLCJpYXQiOjE2NDQ1ODY5NzIsImV4cCI6MTY0NDU5Nzc3Mn0.jpa_rrmB3Hs2xwqQSfbgPRWcoYq0Bz5i4QUnDRrUYBE"
             }
         ).json()
 
-        for article in resposne['result']['list']:
-            if "84" in article['spc2'] :
-                dealOrWarrantPrc = str(article['prcInfo']).split('억')
+        for article in resposne['articleList']:
+            if article['area2'] == 84:
+                dealOrWarrantPrc = str(article['dealOrWarrantPrc']).split('억')
                 firstPrc = int(dealOrWarrantPrc[0]) * 10000
                 secondPrc = 0 if dealOrWarrantPrc[1] == '' else int(str(dealOrWarrantPrc[1]).replace(",", ""))
                 if max < firstPrc + secondPrc:
                     max = firstPrc + secondPrc
-                    minArticle = article
+                    maxArticle = article
         page = page + 1
     return str(max)
 
